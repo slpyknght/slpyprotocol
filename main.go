7844ca4a84508a22c6824e567a618c42b0b956bc
jonas.soehnholz <jonas.soehnholz@deister.com>
2025-04-02 17:13:53 +0200

    added time, destinatio and content length to header

--- main.go
+++ main.go
@@ -3,14 +3,17 @@ package slpyprotocol
 import (
 	"encoding/binary"
 	"fmt"
+	"time"
 )
 
 const(
 	// length in byte
-	VERSION_LEN int = 1
-	TYPE_LEN int = 1
-	DATETIME_LEN int = 4
-	SENDER int = 4
+	VERSION int = 1
+	TYPE int = 1
+	ORIGIN int = 4
+	DESTINATION int = 4
+	DATETIME int = 4
+	CONTENT int = 4
 )
 
 type MessageType int
@@ -22,25 +25,61 @@ const (
 	MT_Connect
 	MT_Disconnect
 	MT_Msg
+	MT_Announcement
 	MT_File
 )
+
+type MessageRaw []byte;
 
 type Message struct{
 	Version byte
 	Type MessageType
-	Sender uint32
+	Origin uint32
+	Destination uint32
+	Datetime uint32
+	ContentLength uint16
 	Content []byte
 }
 
-func CreateMessage(data []byte)(Message,error){
+type ConnectionArgs struct{
+	Name string `json:"name"`
+	Datetime time.Time `json:"datetime"`
+}
+
+func NewMessage(dest uint32, p MessageType)Message{
+	return Message{
+		Version: 1,
+		Type: p,
+		Origin: 0,
+		Destination: dest,
+		Datetime: 1,
+		ContentLength: 0,
+	}
+}
+
+func (data MessageRaw)ToMessage()(Message,error){
 	if data[0] != 0x01{
 		return Message{}, fmt.Errorf("invalid message version: %v", data[0])
 	}
 	msg := Message{Version: data[0]}
 	msg.Type = MessageType(data[1])
-	msg.Sender = binary.LittleEndian.Uint32(data[2:6])
-	msg.Content = data[6:]
+	msg.Origin = binary.LittleEndian.Uint32(data[2:6])
+	msg.Destination = binary.LittleEndian.Uint32(data[6:10])
+	msg.Datetime = binary.LittleEndian.Uint32(data[10:14])
+	msg.ContentLength = binary.LittleEndian.Uint16(data[14:16])
+	msg.Content = data[16:16+msg.ContentLength]
 	return msg, nil
 }
-
+func (msg Message)ToPackage()[]MessageRaw{
+	r := make([]MessageRaw,0)
+	// loop and divide content if > 1024-header bytes to fit into buffer
+	a := []byte{msg.Version, byte(msg.Type)	}
+	a = binary.LittleEndian.AppendUint32(a, msg.Origin)
+	a = binary.LittleEndian.AppendUint32(a, msg.Destination)
+	a = binary.LittleEndian.AppendUint32(a, 1) // time or packacke number
+	a = binary.LittleEndian.AppendUint16(a, msg.ContentLength)
+	a = append(a, msg.Content...)
+	r = append(r, a)
+	return r
+}
 
