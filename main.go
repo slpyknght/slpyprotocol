266e9c9f79d313cca77c4986c3a3b38dc8ffb612
jonas.soehnholz <jonas.soehnholz@deister.com>
2025-04-04 13:06:31 +0200

    add: long message support

--- main.go
+++ main.go
@@ -3,6 +3,8 @@ package slpyprotocol
 import (
 	"encoding/binary"
 	"fmt"
+	"log"
+	"math/rand/v2"
 	"time"
 )
 
@@ -12,8 +14,12 @@ const(
 	TYPE int = 1
 	ORIGIN int = 4
 	DESTINATION int = 4
-	DATETIME int = 4
-	CONTENT int = 4
+	SEQ_LENGTH int = 1
+	SEQ_INDEX int = 1
+	CONTENT_LENGTH int = 4
+	
+	HEADER_SIZE int = 16
+	BUFFER_SIZE int = 128
 )
 
 type MessageType int
@@ -30,13 +36,17 @@ const (
 )
 
 type MessageRaw []byte;
+type MessageBuffer []Message;
 
 type Message struct{
 	Version byte
 	Type MessageType
 	Origin uint32
 	Destination uint32
-	Datetime uint32
+	// Datetime uint32
+	SequenceId byte
+	SequenceLength byte
+	SequenceIndex byte
 	ContentLength uint16
 	Content []byte
 }
@@ -52,9 +62,43 @@ func NewMessage(dest uint32, p MessageType)Message{
 		Type: p,
 		Origin: 0,
 		Destination: dest,
-		Datetime: 1,
 		ContentLength: 0,
+		SequenceIndex: 1,
+		SequenceLength: 1,
+		SequenceId: 1,
 	}
+}
+
+func (msg Message)ToPackage()[]MessageRaw{
+	r := make([]MessageRaw,0)
+	// loop and divide content if > 1024-header bytes to fit into buffer
+	// if msg.ContentLength > uint16(maxLen){
+	// }
+	// contentIdx := 0
+	seqId := make([]byte,4)
+	binary.LittleEndian.PutUint32(seqId, rand.Uint32())
+	var maxLen uint16 = uint16(BUFFER_SIZE - HEADER_SIZE) + 1
+	remainingContent := msg.ContentLength
+	var cidx uint16 = 0
+	var seqLen = 1 + byte(msg.ContentLength / maxLen)
+	for i := byte(0); i < seqLen; i++{
+		var clen uint16
+		clen = min(maxLen, remainingContent)
+		a := []byte{msg.Version, byte(msg.Type)	}
+		a = binary.LittleEndian.AppendUint32(a, msg.Origin)
+		a = binary.LittleEndian.AppendUint32(a, msg.Destination)
+		// a = binary.LittleEndian.AppendUint32(a, 1) // time or packacke number
+		a = append(a, seqId[0], seqLen, (i+1))
+		// fmt.Printf("content len %v / %v",clen, binary.LittleEndian.(clen))
+		a = binary.LittleEndian.AppendUint16(a, clen)
+		a = append(a, msg.Content[cidx:cidx+clen]...)
+		r = append(r, a)
+		cidx += clen
+		remainingContent -= clen
+		fmt.Printf("created package[%v/%v]: %v\n", i+1, seqLen, len(a))
+		fmt.Printf("package header: %v\n", a[:HEADER_SIZE])
+	}
+	return r
 }
 
 func (data MessageRaw)ToMessage()(Message,error){
@@ -65,21 +109,58 @@ func (data MessageRaw)ToMessage()(Message,error){
 	msg.Type = MessageType(data[1])
 	msg.Origin = binary.LittleEndian.Uint32(data[2:6])
 	msg.Destination = binary.LittleEndian.Uint32(data[6:10])
-	msg.Datetime = binary.LittleEndian.Uint32(data[10:14])
-	msg.ContentLength = binary.LittleEndian.Uint16(data[14:16])
-	msg.Content = data[16:16+msg.ContentLength]
+	// msg.Datetime = binary.LittleEndian.Uint32(data[10:14])
+	msg.SequenceId = data[10]
+	msg.SequenceLength = data[11]
+	msg.SequenceIndex = data[12]
+	// fmt.Printf("bytes:%v, uint16: %v\n", data[13:15], binary.LittleEndian.Uint16(data[13:15]))
+	msg.ContentLength = binary.LittleEndian.Uint16(data[13:15])
+	if msg.ContentLength > 0{
+		msg.Content = data[15:15+msg.ContentLength]
+	}
 	return msg, nil
 }
-func (msg Message)ToPackage()[]MessageRaw{
-	r := make([]MessageRaw,0)
-	// loop and divide content if > 1024-header bytes to fit into buffer
-	a := []byte{msg.Version, byte(msg.Type)	}
-	a = binary.LittleEndian.AppendUint32(a, msg.Origin)
-	a = binary.LittleEndian.AppendUint32(a, msg.Destination)
-	a = binary.LittleEndian.AppendUint32(a, 1) // time or packacke number
-	a = binary.LittleEndian.AppendUint16(a, msg.ContentLength)
-	a = append(a, msg.Content...)
-	r = append(r, a)
-	return r
+
+func (buf MessageBuffer)Combine()(Message, error){
+	if len(buf) != int(buf[0].SequenceLength){
+		return Message{}, fmt.Errorf("invalid MessageBuffer length. expected:%d, got:%d", buf[0].SequenceLength, len(buf))
+	}
+	m := Message{
+		Version: buf[0].Version,
+		Type: buf[0].Type,
+		Origin: buf[0].Origin,
+		Destination: buf[0].Destination,
+		SequenceId: buf[0].SequenceId,
+		SequenceLength: buf[0].SequenceLength,
+		ContentLength: 0,
+		Content: make([]byte, 0),
+	}
+	i := byte(0)
+	next := byte(1)
+	missingIndex := false
+	for{
+		if buf[i].SequenceIndex == next{
+			log.Printf("appending %v of %v", next, m.SequenceLength)
+			m.Content = append(m.Content, buf[i].Content...)
+			m.ContentLength += buf[i].ContentLength
+			missingIndex = false
+			next++
+			if next >= m.SequenceLength{
+				break
+			}
+		}
+		i++
+		if i == byte(len(buf)){
+			if missingIndex{
+				break
+			}
+			missingIndex = true
+			i = 0
+		}
+	}
+	if missingIndex{
+		return Message{}, fmt.Errorf("sequence is missing an index: %v", next)
+	}
+	return m,nil
 }
 
